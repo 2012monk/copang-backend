f47de8ce06ad3753419d6891d8a178b3f539793e
monk <2012.monk@gmail.com>
2021-06-17 18:44:32 +0900

    init project

---
+++ src/test/java/com/alconn/copang/CopangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alconn.copang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
