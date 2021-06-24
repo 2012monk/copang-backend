61461fbb16733a034e80b7174fcd8558cded8551
monk <2012.monk@gmail.com>
2021-06-24 14:30:38 +0900

    docs secure

--- src/main/java/com/alconn/copang/controller/DocumentController.java
+++ src/main/java/com/alconn/copang/controller/DocumentController.java
@@ -1,5 +1,6 @@
 package com.alconn.copang.controller;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DocumentController {
 
+    @Secured("ROLE_GUEST")
     @RequestMapping("/docs")
     public String docs() {
         return "redirect:/docs/index.html";
