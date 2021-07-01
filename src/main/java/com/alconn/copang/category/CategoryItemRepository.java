fd275e620ffa70f1eb31cfb97a7ce70e2739fa49
apfp77 <apfp77@naver.com>
2021-07-01 11:38:55 +0900

    categoryitem remove

--- src/main/java/com/alconn/copang/category/CategoryItemRepository.java
+++
@@ -1,6 +0,0 @@
-package com.alconn.copang.category;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryItemRepository extends JpaRepository<CategoryItem, Long> {
-}
