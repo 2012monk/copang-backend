6be1d5b6ee738cfae58d88009f8bd209538927cf
apfp77 <apfp77@naver.com>
2021-07-07 15:40:02 +0900

    부모카테고리 상품등록

--- src/main/java/com/alconn/copang/item/ItemDetailService.java
+++ src/main/java/com/alconn/copang/item/ItemDetailService.java
@@ -67,9 +67,9 @@ public class ItemDetailService {
                 .build();
         Category category=categoryRepository.findById(itemForm.getCategoryId()).orElseThrow(()->new NoSuchElementException("카테고리 정보를 확인해주세요"));
         //==== 자식 카테고리가 있는 상황에 추가시 에러발생``
-        if(category.getChildCheck().equalsIgnoreCase("y")) {
-            throw new DataIntegrityViolationException("자식 카테고리에 등록해주세요");
-        }
+//        if(category.getChildCheck().equalsIgnoreCase("y")) {
+//            throw new DataIntegrityViolationException("자식 카테고리에 등록해주세요");
+//        }
         //====
        //=====
         item.changeCategory(category);
